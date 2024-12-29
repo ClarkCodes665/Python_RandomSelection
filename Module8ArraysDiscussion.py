bbe5d057c4231d3f392e9310871a5dc01c6a838e
Crystal Clark <business.clark665@gmail.com>
2024-12-29 18:07:11 -0500

    Add Game Night random selector program

---
+++ Module8ArraysDiscussion.py
@@ -0,0 +1,15 @@
+import random
+print("Welcome to Game Night!\n")
+print("Let's see who's picking tonight's game...")
+
+players = ["Bill", "Roni", "Ryan", "Evan","Clark"]
+
+
+winner=random.choice(players)
+
+print("\nTonight's lucky game picker is " + winner + "!")
+
+print("\nCongrat's " + winner + "!") 
+
+print(players[2])
+print(players[4])
